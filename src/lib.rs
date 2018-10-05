74d64b3ff665f4e6f3f578a84944b96c2a641845
Connor Gray <accounts@connorgray.com>
2018-10-05 00:58:39 -0500

    Moved comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-// The name of this file comes from `build.rs`.
 #![allow(
     non_snake_case,
     non_upper_case_globals,
     non_camel_case_types,
 )]
+// The name of this file comes from `build.rs`.
 include!(concat!(env!("OUT_DIR"), "/LibraryLink_bindings.rs"));
