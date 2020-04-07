3e6848545309b6bc04afeedff3ff7b1f10cb5f0b
Connor Gray <accounts@connorgray.com>
2020-04-07 10:20:32 -0500

    Add basic tests that generate_wrapper![] doesn't produce an error

---
+++ tests/main.rs
@@ -0,0 +1,19 @@
+use wl_library_link::{Engine, generate_wrapper};
+
+use wl_expr::Expr;
+
+// #[test]
+// fn main() {
+//     // WRAPPED_ONE(5);
+// }
+
+generate_wrapper![WRAPPED_ONE # wrapped_one(a: Expr) -> Expr];
+generate_wrapper![WRAPPED_TWO # wrapped_two(engine: Engine, a: Expr) -> Expr];
+
+fn wrapped_one(a: Expr) -> Expr {
+    a
+}
+
+fn wrapped_two(engine: Engine, a: Expr) -> Expr {
+    a
+}
