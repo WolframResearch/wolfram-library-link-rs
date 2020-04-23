037017264c6263f7e0544b1cf86c688a5def686d
Connor Gray <accounts@connorgray.com>
2020-04-23 16:50:17 -0500

    Add example of using WSTPLink from unsafe bindings to mix low-level and high-level bindings

--- examples/raw_wstp_function.rs
+++ examples/raw_wstp_function.rs
@@ -8,6 +8,16 @@ use wl_library_link::sys::{
 };
 use wl_wstp::sys::{WSGetInteger, WSNewPacket, WSPutInteger, WSTestHead, WSLINK};
 
+/// This function is loaded by evaluating:
+///
+/// ```wolfram
+/// LibraryFunctionLoad[
+///     "/path/to/libraw_wstp_function.dylib",
+///     "demo_wstp_function",
+///     LinkObject,
+///     LinkObject
+/// ]
+/// ```
 #[no_mangle]
 pub unsafe extern "C" fn demo_wstp_function(
     _lib: WolframLibraryData,
@@ -39,6 +49,56 @@ pub unsafe extern "C" fn demo_wstp_function(
     if WSPutInteger(link, sum) == 0 {
         return LIBRARY_FUNCTION_ERROR;
     }
+
+    return LIBRARY_NO_ERROR;
+}
+
+/// This example shows how the raw Rust wrappers can be mixed with higher-level wrappers
+/// around the Wolfram Symbolic Transfer Protocal (WSTP) for conveniently calling back
+/// into the Kernel to perform an evaluation.
+///
+/// This function is loaded by evaluating:
+///
+/// ```wolfram
+/// LibraryFunctionLoad[
+///     "/path/to/libraw_wstp_function.dylib",
+///     "demo_wstp_function_callback",
+///     LinkObject,
+///     LinkObject
+/// ]
+/// ```
+#[no_mangle]
+pub extern "C" fn demo_wstp_function_callback(
+    lib: WolframLibraryData,
+    link: WSLINK,
+) -> c_uint {
+    let link = unsafe { WSTPLink::new(link) };
+
+    link.get_expr().unwrap();
+
+    let callback_link = unsafe { (*lib).getWSLINK.unwrap()(lib) };
+
+    {
+        let safe_callback_link = unsafe { WSTPLink::new(callback_link as *mut _) };
+
+        safe_callback_link
+            .put_expr(&Expr! {
+                EvaluatePacket[Print["Hello, World! --- WSTP"]]
+            })
+            .unwrap();
+
+        unsafe {
+            (*lib).processWSLINK.unwrap()(callback_link);
+        }
+
+        // FIXME: Is this necessary? -- Answer, yes, otherwise the link has unread data.
+        if let Err(message) = safe_callback_link.get_expr() {
+            link.put_expr(&Expr::string(message)).unwrap();
+            return LIBRARY_NO_ERROR;
+        }
+    }
+
+    link.put_expr(&Expr::string("returned normally")).unwrap();
 
     return LIBRARY_NO_ERROR;
 }
