e1ddf8f22943dcc1ac1c6be3cd0a4da4e1fe4c47
Connor Gray <connorg@wolfram.com>
2020-05-05 08:41:02 -0500

    Add PacletInfo.m and docbuild.xml, used by Workbench for building the docs

---
+++ PacletInfo.m
@@ -0,0 +1,15 @@
+(* Paclet Info File *)
+
+(* created 2020/05/04*)
+
+Paclet[
+    Name -> "LibraryLinkForRust",
+    Version -> "0.0.1",
+    MathematicaVersion -> "6+",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
