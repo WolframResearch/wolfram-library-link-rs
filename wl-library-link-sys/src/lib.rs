67b3682e36471063d25ed726a3b4d94bf03900f8
Connor Gray <connorg@wolfram.com>
2021-11-24 10:41:51 -0600

    cleanup: Rename `build` directory to `generated` directory to avoid confusion

--- wl-library-link-sys/src/lib.rs
+++ wl-library-link-sys/src/lib.rs
@@ -3,4 +3,4 @@
 #![allow(non_snake_case, non_upper_case_globals, non_camel_case_types)]
 
 // The name of this file comes from `build.rs`.
-include!("../build/LibraryLink_bindings.rs");
+include!("../generated/LibraryLink_bindings.rs");
