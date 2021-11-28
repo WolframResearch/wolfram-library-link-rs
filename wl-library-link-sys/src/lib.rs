b8825a48266fe48c28aef69d1c7d82a28ba6ea4a
Connor Gray <connorg@wolfram.com>
2021-11-28 17:34:53 -0600

    chore: silence deref_nullptr warning generated by the bindgen tests

--- wl-library-link-sys/src/lib.rs
+++ wl-library-link-sys/src/lib.rs
@@ -1,6 +1,7 @@
 //! Automatically generated bindings to the Wolfram LibraryLink C API.
 
 #![allow(non_snake_case, non_upper_case_globals, non_camel_case_types)]
+#![allow(deref_nullptr)]
 
 // The name of this file comes from `build.rs`.
 include!("../generated/LibraryLink_bindings.rs");
