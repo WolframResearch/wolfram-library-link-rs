7ca66474eb53c71752fedcb01718f45b153ac93d
Connor Gray <ConnorGray@users.noreply.github.com>
2022-01-11 21:11:45 -0600

    cleanup: Remove old test + polish in managed.rs (#11)
    * cleanup: Remove old libstructured_expressions test
    
    The "structured expressions" feature was removed to streamline the
    scope of wolfram-library-link in preparation for release. This test
    may come back at some point, but for now it's just failing the test
    suite pointlessly.
    
    * docs: Wording improvements in internal implementation comment in managed.rs
    
    * fixup: Make the number of elements in SLOTS match the doc comment
    
    * cleanup: replace unnecessary `const` parameter with normal parameter + rename for clarity

--- RustLink/Examples/Expressions.wlt
+++ RustLink/Examples/Expressions.wlt
@@ -21,24 +21,3 @@ TestMatch[
 		"Backtrace" -> Missing["NotEnabled"]
 	|>]
 ]
-
-TestMatch[
-	func = LibraryFunctionLoad[
-		"libstructured_expressions",
-		"sum_of_numbers",
-		LinkObject,
-		LinkObject
-	];
-
-	func[1, 2, 3]
-	,
-	(* FIXME: This output is a bug. Fix the bug and update this test case. *)
-	Failure["RustPanic", <|
-		"MessageTemplate" -> "Rust LibraryLink function panic: `message`",
-		"MessageParameters" -> <|
-			"message" -> "WstpFunction: WSTP error reading argument List expression: WSTP error: symbol name 'List' has no context"
-		|>,
-		"SourceLocation" -> s_?StringQ /; StringStartsQ[s, "wolfram-library-link/src/args.rs:"],
-		"Backtrace" -> Missing["NotEnabled"]
-	|>]
-]
--- wolfram-library-link/src/managed.rs
+++ wolfram-library-link/src/managed.rs
@@ -105,7 +105,7 @@ pub fn register_library_expression_manager(
 ///
 /// This specific problem is an instance of the more general problem of how to expose a
 /// user-provided function/closure (non-`extern "C"`) as-if it actually were an
-/// `extern "C"` function. one common strategy is to use a "trampline" function
+/// `extern "C"` function.
 ///
 /// There are two common ways we could concievably do this:
 ///
@@ -126,35 +126,39 @@ pub fn register_library_expression_manager(
 /// case, because their is no way to pass a custom argument to the callback expected by
 /// registerLibraryExpressionManager().
 ///
+/// In both the (1.) and (2.) strategies, the solution is to create a single wrapper
+/// function for each user function, which is hard-coded to call the user function that
+/// it wraps.
+///
 /// The technique used here is a third strategy:
 ///
 /// 3. Store the user-provided function pointer into a static array, and, instead of
-///    having a single `extern "C"` wrapper functions, have multiple `extern "C"` wrapper
+///    having a single `extern "C"` wrapper function, have multiple `extern "C"` wrapper
 ///    functions, each of which statically access a different index in the static array.
 ///
-///    By having different `extern "C"` functions that access different static data, we
+///    By using different `extern "C"` functions that access different static data, we
 ///    can essentially "fake" having an extra function argument that we control.
 ///
 ///    This depends on the observation that the callback function pointer is itself a
 ///    value we control.
 ///
 ///    This technique is limited by the fact that the static function pointers must be
-///    declared ahead of time, and so we must place a somewhat arbitrary limit on
-///    how many callbacks can be registered at a time.
+///    declared ahead of time (see `def_slot_fn!` below), and so practically there is a
+///    somewhat arbitrary limit on how many callbacks can be registered at a time.
 ///
-/// In our case, the *only* data we can register is the static function pointer itself;
-/// so strategy (3.) is the way to go.
+/// In our case, the *only* data we are able pass through the C API is the static function
+/// pointer we are registering; so strategy (3.) is the way to go.
 ///
-/// SLOTS has 8 elements, and we define 8 `extern "C" fn slot_<X>(..)` functions which
-/// access only the corresponding element in SLOTS.
+/// `SLOTS` has 8 elements, and we define 8 `extern "C" fn slot_<X>(..)` functions that
+/// access only the corresponding element in `SLOTS`.
 ///
 /// 8 was picked arbitrarily, on the assumption that 8 different registered types should
 /// be sufficient for the vast majority of libraries. Libraries that want to register more
 /// than 8 types can use `rtl::registerLibraryExpressionManager` directly as a workaround.
 ///
 /// TODO: Also store the "name" of this manager, and pass it to the user function?
-static SLOTS: Lazy<Mutex<[Option<fn(ManagedExpressionEvent)>; 4]>> =
-    Lazy::new(|| Mutex::new([None, None, None, None]));
+static SLOTS: Lazy<Mutex<[Option<fn(ManagedExpressionEvent)>; 8]>> =
+    Lazy::new(|| Mutex::new([None; 8]));
 
 fn register_using_next_slot(name: &str, manage_instance: fn(ManagedExpressionEvent)) {
     let name_cstr = CString::new(name).expect("failed to allocate C string");
@@ -171,6 +175,7 @@ fn register_using_next_slot(name: &str, manage_instance: fn(ManagedExpressionEve
         *slot = Some(manage_instance);
         register_using_slot(name_cstr, index)
     } else {
+        // Drop `slots` to avoid poisoning SLOTS when we panic.
         drop(slots);
         panic!("maxiumum number of library expression managers have been registered");
     };
@@ -214,10 +219,10 @@ fn register_using_slot(name_cstr: CString, index: usize) -> Result<(), ()> {
 // Static slot_<X> functions
 //--------------------------
 
-fn call_using_slot<const SLOT: usize>(mode: sys::mbool, id: sys::mint) {
+fn call_callback_in_slot(slot: usize, mode: sys::mbool, id: sys::mint) {
     let slots = SLOTS.lock().unwrap();
 
-    let user_fn: fn(ManagedExpressionEvent) = match slots[SLOT] {
+    let user_fn: fn(ManagedExpressionEvent) = match slots[slot] {
         Some(func) => func,
         // TODO: Set something like "RustLink`$LibraryLastError" with a descriptive error?
         None => return,
@@ -253,7 +258,7 @@ macro_rules! def_slot_fn {
             mode: sys::mbool,
             id: sys::mint,
         ) {
-            call_using_slot::<$index>(mode, id)
+            call_callback_in_slot($index, mode, id)
         }
     };
 }
